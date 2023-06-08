9653d3e954be40de567b1f5b21ea9ccc213d5924
Rinat Sarmuldin <ura07srr@gmail.com>
2023-06-08 10:57:22 +0500

    entering api info

--- main.js
+++ main.js
@@ -4,5 +4,18 @@ async function getPosts(){
     let res = await fetch('https://jsonplaceholder.typicode.com/posts');
     let posts = await res.json();
 
-    console.log(posts);
+
+    // вывод через разметку данных с apiS
+    posts.forEach((post) => {
+        document.querySelector('.post-list') .innerHTML += `
+        <div class="card" style="...">
+            <div class="card-body">
+                <h5 class="cart-title">${post.title}</h5>
+                <p class="card-text">${post.body}</p>          
+                <a href="#" class="card-link">Подробнее</a>
+            </div>
+        </div> 
+        `
+    })
 }
+getPosts();
